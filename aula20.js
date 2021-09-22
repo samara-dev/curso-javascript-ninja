e889432be0eee9ad1b46f78b5d68ad8e30ecff6b
Samara Santos <samarasantossoares@gmail.com>
2021-09-22 09:15:19 -0300

    alterando arquivo html

---
+++ aula20.js
@@ -0,0 +1,18 @@
+(function(win, doc) {
+    'use strict';
+    // alert() - Retorna uma mensagem na tela.
+    // confirm() - retorna um booleano.
+    // prompt() - Entrada de dados.
+    // window - representa a janela do browser.
+    // getElementById() - pega um elemento através do id.
+    // getElementsByClassName() - pega uma elemento a partir de uma classe.
+    // getElementsByTagName() - pega elementos através de tags.
+    // getElementsByName() - pega elementos através do nome.
+
+    // Não causam efeitos colaterais:
+    // querySelector() - faz a seleção do primeiro elemento a partir de uma estrutura css.
+    // querySelectorAll() - faz a seleção de todos os elementos a partir de uma estrutura css.
+    
+
+
+})(window, document);
